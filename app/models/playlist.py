df5faf9ef7d6fd4be3e1a0f4a419f812fc9961e9
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-01-15 15:19:43 -0800

    creating a M2M relationship in progress

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,5 +1,14 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
+playlist_songs = db.Table(
+    'playlist-songs',
+    db.Model.metadata,
+    db.Column('playlist_id', db.Integer, db.ForeignKey(add_prefix_for_prod("playlists.id"))),
+    db.Column('song_id', db.Integer, db.ForeignKey(add_prefix_for_prod("songs.id")))
+)
+
+if environment == "production":
+    playlist_songs.schema = SCHEMA
 
 
 
