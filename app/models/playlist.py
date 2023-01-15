d7b6c9fb2c5477725c851e0b77b3fed45d4c3a1a
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-01-15 08:56:11 -0800

    playlist model

---
+++ app/models/playlist.py
@@ -0,0 +1,30 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Playlist(db.Model):
+    __tablename__='playlists'
+
+    if environment == "production":
+         __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.Date, nullable=False)
+    updated_at = db.Column(db.Date, nullable=False) 
+
+
+
+##one to many relationship every user can create many playlists
+    user = db.relationship("User", back_populates='playlists')
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'title':self.title,
+            'description':self.description,
+            'image_url':self.image_url,
+            'user':self.user.to_dict() ## need to check 
+        }
