50e6679970aa7f678e980b3a59a39bc38dae97e1
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-01-15 09:46:11 -0800

    songs model

--- app/models/song.py
+++ app/models/song.py
@@ -1,15 +1,22 @@
-from .db import db
-
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Song(db.Model):
     __tablename__ = "songs"
 
-    
+    if environment == "production":
+         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    url = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    song_url = db.Column(db.String)
+    album_id = db.Column(db.String)
+    artist_id = db.Column(db.String, db.ForeignKey(add_prefix_for_prod('albums.id')))
+    ## maybe song length
+
+    ##relationship 
+    album = db.relationship('Album', back_populates='songs')
+
 
     # user = db.relationship("User", back_populates="images")
 
