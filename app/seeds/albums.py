3cbbeb685dcff01912878518f964a38f69379203
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-01-16 11:37:13 -0800

    add seed data from albums and songs

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -1,13 +1,40 @@
 from app.models import db, environment, SCHEMA, Album
 
 def seed_albums():
-    demo_album = Album(
-       artist ='Demo',
+    demo_album1 = Album(
+       artist ='Demo1',
        album_img_url = 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
-       title = 'PAIN'
+       title = 'PAIN1'
+    )
+    demo_album2 = Album(
+       artist ='Demo2',
+       album_img_url = 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
+       title = 'PAIN2'
+    )
+    demo_album3 = Album(
+       artist ='Demo3',
+       album_img_url = 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
+       title = 'PAIN3'
+    )
+    demo_album4 = Album(
+       artist ='Demo4',
+       album_img_url = 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
+       title = 'PAIN4'
+    )
+    demo_album5 = Album(
+       artist ='Demo5',
+       album_img_url = 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440',
+       title = 'PAIN5'
     )
 
-    db.session.add(demo_album)
+
+
+    
+    db.session.add(demo_album1)
+    db.session.add(demo_album2)
+    db.session.add(demo_album3)
+    db.session.add(demo_album4)
+    db.session.add(demo_album5)
     db.session.commit()
 
 def undo_albums():
--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -1,14 +1,42 @@
 from app.models import db, environment, SCHEMA, Song
 
 def seed_songs():
-    demo_song = Song(
-        title = 'Peaceful Piano',
+    demo_song1 = Song(
+        title = 'Peaceful Piano1',
         album_id = 1,
         song_url = 'https://spotify8bucket.s3.amazonaws.com/3c11c9105e734eaf906a4dac2fc52b16.mp3' 
 
     )
+    demo_song2 = Song(
+        title = 'Peaceful Piano2',
+        album_id = 2,
+        song_url = 'https://spotify8bucket.s3.amazonaws.com/fingerstyle-guitar-cover.mp3' 
 
-    db.session.add(demo_song)
+    )
+    demo_song3 = Song(
+        title = 'Peaceful Piano3',
+        album_id = 3,
+        song_url = 'https://spotify8bucket.s3.amazonaws.com/you-proof-official-music-video.mp3' 
+
+    )
+    demo_song4 = Song(
+        title = 'Peaceful Piano4',
+        album_id = 4,
+        song_url = 'https://spotify8bucket.s3.amazonaws.com/payphone-fingerstyle-guitar-cover-by-james-bartholomew.mp3' 
+
+    )
+    demo_song5 = Song(
+        title = 'Peaceful Piano5',
+        album_id = 5,
+        song_url = 'https://spotify8bucket.s3.amazonaws.com/jhoom-x-kesariya-acv-mashup-brahmastra-ranbir-kapoor-alia-bhatt-arijit-singh-ali-zafar.mp3' 
+
+    )
+
+    db.session.add(demo_song1)
+    db.session.add(demo_song2)
+    db.session.add(demo_song3)
+    db.session.add(demo_song4)
+    db.session.add(demo_song5)
     db.session.commit()
 
 def undo_songs():
