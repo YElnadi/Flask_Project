2b38c5ab53e2aa803802b6c13438613ed8acfd7b
Eric Mathews <eam34m@gmail.com>
2023-01-19 12:33:05 -0800

    finished continuous playlists

--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -4,31 +4,31 @@ def seed_songs():
     demo_song1 = Song(
         title = 'Peaceful Piano1',
         album_id = 1,
-        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/ziyounvshen.mp3' 
+        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/ziyounvshen.mp3'
 
     )
     demo_song2 = Song(
         title = 'Peaceful Piano2',
         album_id = 2,
-        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3' 
+        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3'
 
     )
     demo_song3 = Song(
         title = 'Peaceful Piano3',
         album_id = 3,
-        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3' 
+        song_url = 'https://spotify8bucket.s3.amazonaws.com/3c11c9105e734eaf906a4dac2fc52b16.mp3'
 
     )
     demo_song4 = Song(
         title = 'Peaceful Piano4',
         album_id = 4,
-        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3' 
+        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3'
 
     )
     demo_song5 = Song(
         title = 'Peaceful Piano5',
         album_id = 5,
-        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/suipian.mp3' 
+        song_url = 'https://hanzluo.s3-us-west-1.amazonaws.com/music/suipian.mp3'
 
     )
 
@@ -40,12 +40,12 @@ def seed_songs():
     db.session.add(demo_song5)
     db.session.commit()
     songs = [demo_song1, demo_song2, demo_song3, demo_song4, demo_song5]
-    return songs 
+    return songs
 
 def undo_songs():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.songs RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM songs")
-        
+
     db.session.commit()
--- react-app/src/components/ContinuousPlayer.js
+++ react-app/src/components/ContinuousPlayer.js
@@ -2,65 +2,42 @@ import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import { useSelector } from "react-redux";
 import { useState } from "react";
-// // import 'react-h5-audio-player/lib/styles.less' Use LESS
-// // import 'react-h5-audio-player/src/styles.scss' Use SASS
-
-// const Player = () => {
-//   const queue = useSelector((state) => state.songs.queue);
-//   const [currentTrack, setCurrentTrackIndex] = useState(0);
-
-//   return (
-//     <AudioPlayer
-//       autoPlay
-//       src="https://spotify8bucket.s3.amazonaws.com/3c11c9105e734eaf906a4dac2fc52b16.mp3"
-//       onPlay={(e) => console.log("onPlay")}
-//       // other props here
-//     />
-//   );
-// };
-
-// export default Player;
-
-//////////////////////////////////////////////////
-const playlist = [
-  { src: 'https://hanzluo.s3-us-west-1.amazonaws.com/music/ziyounvshen.mp3' },
-  { src: 'https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3' },
-  { src: 'https://hanzluo.s3-us-west-1.amazonaws.com/music/suipian.mp3' },
-]
 
 const Player = () => {
-  const [currentTrack, setTrackIndex] = useState(0)
+  const queue = useSelector((state) => state.songs.queue);
+  const [currentTrack, setTrackIndex] = useState(0);
   const handleClickNext = () => {
-      console.log('click next')
-        setTrackIndex((currentTrack) =>
-            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
-        );
-    };
-  
-  const handleEnd = () => {
-    console.log('end')
+    console.log("click next");
     setTrackIndex((currentTrack) =>
-            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
-        );
-  }
-  return (
-      <div className="container">
-        <h1>Hello, audio player!</h1>
-        <AudioPlayer
-          volume="0.5"
-          src={playlist[currentTrack].src}
-          showSkipControls
-          onClickNext={handleClickNext}
-          onEnded={handleEnd}
-          // Try other props!
-        />
-        <p>
-          <a href="https://github.com/lhz516/react-h5-audio-player" target="_blank">See Docs on Github</a>
-         </p>
-      </div>
+      currentTrack < queue.length - 1 ? currentTrack + 1 : 0
     );
-}
+  };
 
-
+  const handleEnd = () => {
+    console.log("end");
+    setTrackIndex((currentTrack) =>
+      currentTrack < queue.length - 1 ? currentTrack + 1 : 0
+    );
+  };
+  if (queue.length) {
+    return (
+      <AudioPlayer
+        volume="0.5"
+        src={queue[currentTrack].song_url}
+        showSkipControls
+        onClickNext={handleClickNext}
+        onEnded={handleEnd}
+        // Try other props!
+      />
+    );
+  } else {
+    return (
+      <AudioPlayer
+        src={""}
+        // Try other props!
+      />
+    );
+  }
+};
 
 export default Player;
