cff858a64d61fb56c5db9604ff04f7ffe5d690ed
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-01-22 10:35:53 -0800

    styling for the home page and albums

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import { logout } from '../../store/session';
@@ -6,8 +7,10 @@ import { logout } from '../../store/session';
 const LogoutButton = () => {
   //const hover = useHover({backgroundColor: "LightBlue"})  
   const dispatch = useDispatch()
+  const history = useHistory()
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/')
   };
 
   return <button onClick={onLogout} style ={{borderRadius:'20px' , padding:10, borderColor:'whitesmoke', backgroundColor:'transparent', color:'whitesmoke', cursor:'pointer'}}>Log out</button>;
